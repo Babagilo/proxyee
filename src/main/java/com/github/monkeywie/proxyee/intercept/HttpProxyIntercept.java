69aaaa4ee25190ecb14717e0e9e2d8949c5a4a11
LI Yong <yong.li@soprasteria.com>
2018-11-21 09:36:24 +0800

    Translation comments to English

--- src/main/java/com/github/monkeywie/proxyee/intercept/HttpProxyIntercept.java
+++ src/main/java/com/github/monkeywie/proxyee/intercept/HttpProxyIntercept.java
@@ -9,6 +9,7 @@ public class HttpProxyIntercept {
 
   /**
    * 拦截代理服务器到目标服务器的请求头
+   * Intercept the request header of Proxy-->Origin
    */
   public void beforeRequest(Channel clientChannel, HttpRequest httpRequest,
       HttpProxyInterceptPipeline pipeline) throws Exception {
@@ -17,6 +18,7 @@ public class HttpProxyIntercept {
 
   /**
    * 拦截代理服务器到目标服务器的请求体
+   * Intercept the request body of Proxy --> Origin
    */
   public void beforeRequest(Channel clientChannel, HttpContent httpContent,
       HttpProxyInterceptPipeline pipeline) throws Exception {
@@ -25,6 +27,7 @@ public class HttpProxyIntercept {
 
   /**
    * 拦截代理服务器到客户端的响应头
+   * Intercept the response header of Client <-- Proxy
    */
   public void afterResponse(Channel clientChannel, Channel proxyChannel, HttpResponse httpResponse,
       HttpProxyInterceptPipeline pipeline) throws Exception {
@@ -34,6 +37,7 @@ public class HttpProxyIntercept {
 
   /**
    * 拦截代理服务器到客户端的响应体
+   * Intercept the response body of Client <-- Proxy
    */
   public void afterResponse(Channel clientChannel, Channel proxyChannel, HttpContent httpContent,
       HttpProxyInterceptPipeline pipeline)
